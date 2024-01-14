343d4f8727018610325d201aae87ddae05fdbd4a
vxgx <vxgx@akasecs-iMac.local>
2024-01-14 13:46:52 +0100

    i sleepy

---
+++ javaModule00/ex00/Program.java
@@ -0,0 +1,11 @@
+class Program {
+	public static void main(String[] args) {
+		int	number = 479598;
+		int output = 0;
+		while(number != 0) {
+			output += number % 10;
+			number /= 10;
+		}
+		System.out.println(output);
+	}
+}

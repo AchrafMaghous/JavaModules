b51a3f61cfcaf09e8a7b45d22174287c310053ee
achrafmaghous <muteallfocus7@gmail.com>
2024-09-15 13:35:52 +0100

    redone them

--- javaModule00/ex03/Program.java
+++ javaModule00/ex03/Program.java
@@ -1,94 +1,64 @@
-package ex03;
 
 import java.util.Scanner;
+import java.util.TreeMap;
 
-/**
- * The Program class represents a program that processes student grades data and draws a chart based on the data.
- * It contains methods for getting grades, drawing a chart, and the main method to run the program.
- */
-/**
- * The Program class represents a program that processes student grades data and draws a chart based on the data.
- */
+
 public class Program {
-	
-	/**
-	 * Retrieves the minimum grade from a string of digits and updates the studentsData.
-	 * 
-	 * @param digits The string of digits representing the grades.
-	 * @param studentsData The current studentsData.
-	 * @return The updated studentsData with the minimum grade appended.
-	 */
-	private static long getGrades(String digits, long studentsData) {
-		Scanner input = new Scanner(digits);
-		int digitC = 0;
-		int minGrade = 9;
+  public static void main(String[] args) {
+    Scanner skarner = new Scanner(System.in);
+    TreeMap<Integer, Integer> weekData = new TreeMap<>();
+    int weekCounter = 1;
 
-		input.useDelimiter(" ");
-		while (input.hasNext() && digitC < 5) {
-			if (input.hasNextInt()) {
-				int digit = input.nextInt();
-				minGrade = (digit < minGrade) ? digit : minGrade;
-				if (digit < 1 || digit > 9) {
-					System.err.println("bad input");
-					System.exit(-1);
-				}
-			} else
-				break;
-			digitC++;
-		}
-		if (digitC != 5 || input.hasNext()) {
-			System.err.println("bad input");
-			System.exit(-1);
-		}
-		input.close();
-		return (studentsData * 10 + minGrade);
-	}
-	
-	/**
-	 * Draws a chart based on the studentsData and the total number of weeks.
-	 * 
-	 * @param studentsData The studentsData containing the grades.
-	 * @param totalWeeks The total number of weeks.
-	 */
-	private static void drawChart(long studentsData, int totalWeeks) {
-		int weekCount = 1;
+    while (skarner.hasNext()) {
+      String line = skarner.nextLine();
+      if (line.equals("42"))
+        break;
+      if (line.startsWith("Week ")) {
+        int week;
+        week = Integer.parseInt(line.substring(5).trim());
+        if (week < 1 || week > 18) {
+          System.err.println("IllegalArgument");
+          System.exit(-1);
+        }
+        if (week != weekCounter) {
+          System.err.println("IllegalArgument");
+          System.exit(-1);
+        }
 
-		long division = (long) Math.pow(10, totalWeeks - 2);
-		while (division > 0) {
-			System.out.print("Week " + weekCount + " ");
+        int[] grades = new int[5];
+        int i = 0;
+        while (i < 5 && skarner.hasNextInt()) {
+          grades[i] = skarner.nextInt();
+          i++;
+        }
+        skarner.nextLine();
+        if (i != 5) {
+          System.err.println("IllegalArgument");
+          System.exit(-1);
+        }
+        int minGrade = 10;
+        for (int grade : grades) {
+          if (grade < 1 || grade > 9) {
+            System.err.println("IllegalArgument");
+            System.exit(-1);
+          }
+          if (grade < minGrade)
+            minGrade = grade;
 
-			int digit = (int) (studentsData / division);
-			studentsData = studentsData % division;
-			division = division / 10;
-			for (int j = 0; j < digit; j++)
-				System.out.print("=");
-			System.out.println(">");
-			weekCount++;
-		}
-	}
-	
-	/**
-	 * The main entry point of the program.
-	 * 
-	 * @param args The command line arguments.
-	 */
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		long studentsData = 0;
-		int currWeek = 1;
-		while (true) {
-			String weekStr = input.nextLine();
-			if (weekStr.equals("42") || currWeek > 18)
-				break;
-			if (!weekStr.equals("Week " + currWeek)) {
-				System.err.println("Wrong input");
-				System.exit(-1);
-			}
-			String digits = input.nextLine();
-			studentsData = getGrades(digits, studentsData);
-			currWeek++;
-		}
-		input.close();
-		drawChart(studentsData, currWeek);
-	}
+        }
+        weekData.put(week, minGrade);
+        weekCounter += 1;
+      }
+    }
+    for (int week : weekData.keySet()) {
+      int minGrade = weekData.get(week);
+      System.out.print("Week " + week + " ");
+      for (int i = 0; i < minGrade; i++) {
+        System.out.print("=");
+      }
+      System.out.println(">");
+    }
+    skarner.close();
+
+  }
 }
